59c651142e895ae82f4eb85b6c2b2bf3cf5291a4
corpltd <89472911+corpltd@users.noreply.github.com>
2022-09-06 12:55:42 +0200

    🔴 romanNumber 1

--- src/test/java/YatziTest.java
+++ src/test/java/YatziTest.java
@@ -1,9 +1,20 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class YatziTest {
+
     @Test
     void TestOne(){
-        System.out.println("hello");
+
+        assertEquals("I", toRoman(1));
+
+    }
+
+    private String toRoman(int i) {
+
+        return null;
+
     }
 
 }
